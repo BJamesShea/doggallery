c9a3e878b4fb68a5a513f2277fe0c9e6c2aa2c61
Brandon Shea <brandon.shea1994@icloud.com>
2024-07-17 23:11:45 -0230

    Small changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt,
+        optio.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
